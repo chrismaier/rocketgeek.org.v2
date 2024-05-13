04368ac36d82660653867166723ac568e3048074
Chris Maier <maier.chris@gmail.com>
2024-05-13 00:36:17 -0500

    Begin work on include files and making the css and other common objects reusable

---
+++ assets/js/css-include-script.js
@@ -0,0 +1,20 @@
+fetch('./assets/json-data/css-include-list.json')
+    .then(response => response.json())
+    .then(data => {
+        // Function to add CSS files dynamically to the head section
+        function addCSSFiles(cssIncludeFiles) {
+            cssIncludeFiles.forEach(function(cssFile) {
+                let link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = cssFile;
+                document.head.appendChild(link);
+            });
+        }
+        
+        // List of CSS files from JSON
+        let cssIncludeFiles = data.cssIncludeFiles;
+        
+        // Add CSS files dynamically
+        addCSSFiles(cssIncludeFiles);
+    })
+    .catch(error => console.error('Error fetching styles.json:', error));
