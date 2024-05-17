ff90740adf3750fcf5d7afab6efac64742941a0f
Chris Maier <maier.chris@gmail.com>
2024-05-17 00:45:40 -0500

    Fine tuning of side nav bar

---
+++ assets/js/side-nav-aarow-button.js
@@ -0,0 +1,10 @@
+// JavaScript for toggling sidebar
+document.getElementById('sidebarToggle').addEventListener('click', function ()
+{
+    document.getElementById('sidebar').classList.toggle('active');
+    // Toggle the arrow icon
+    document.getElementById('icon-toggle').classList.toggle('fa-arrows-left-right-to-line');
+    //document.getElementById('icon-toggle').classList.toggle('fa-arrow-right');
+});
+
+//<i class="fa-solid fa-arrows-left-right-to-line"></i>
