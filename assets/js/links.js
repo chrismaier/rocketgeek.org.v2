5224c8e020548d32465f346b2bc7cb36a743ea55
Chris Maier <maier.chris@gmail.com>
2024-06-03 01:54:33 -0500

    Adding links page and optimizing pages that have not been updated to bootstrap 5. Altered the robots.txt file

---
+++ assets/js/links.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('assets/json-data/other-links.json')
+        .then(response => response.json())
+        .then(data => {
+            const linkContainer = document.querySelector('#link-container');
+            data.forEach(link => {
+                const card = document.createElement('div');
+                card.className = 'link-card';
+                
+                card.onclick = () => {
+                    window.open(link.url, '_blank');
+                };
+                
+                const title = document.createElement('h3');
+                title.className = 'link-title';
+                title.textContent = link.title;
+                
+                const url = document.createElement('p');
+                url.className = 'link-url';
+                url.textContent = link.url;
+                
+                const description = document.createElement('p');
+                description.className = 'link-description';
+                description.textContent = link.description;
+                
+                card.appendChild(title);
+                card.appendChild(url);
+                card.appendChild(description);
+                linkContainer.appendChild(card);
+            });
+        })
+        .catch(error => console.error('Error fetching the links:', error));
+});
