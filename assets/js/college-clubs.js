9a79fe954cf6fe36ecd372a18ed1ab74edbb6fb5
Chris Maier <maier.chris@gmail.com>
2025-07-26 15:28:06 -0500

    Updating the clubs page to include two columns. Minor other navigation enhancements included

---
+++ assets/js/college-clubs.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    console.log("Loading College & HS Clubs");
+    
+    $.getJSON("/assets/json-data/college-clubs.json", function (data) {
+        console.log("College Clubs JSON loaded", data);
+        
+        data.forEach(function (club) {
+            let teamNumberLine = '';
+            if (club.team_number) {
+                if (typeof club.team_number === 'object' && club.team_number.value) {
+                    if (club.team_number.link) {
+                        teamNumberLine = `<li><strong>Team Number:</strong> <a href="${club.team_number.link}" target="_blank">${club.team_number.value}</a></li>`;
+                    } else {
+                        teamNumberLine = `<li><strong>Team Number:</strong> ${club.team_number.value}</li>`;
+                    }
+                } else if (typeof club.team_number === 'string') {
+                    teamNumberLine = `<li><strong>Team Number:</strong> ${club.team_number}</li>`;
+                }
+            }
+            
+            let associatedLaunchClubsLine = '';
+            if (club.associated_launch_clubs && club.associated_launch_clubs.length > 0) {
+                associatedLaunchClubsLine = `<li><strong>Launch Club(s):</strong> `;
+                associatedLaunchClubsLine += club.associated_launch_clubs.map(lc => {
+                    return `<a href="${lc.website}" target="_blank">${lc.name}</a>`;
+                }).join(", ");
+                associatedLaunchClubsLine += `</li>`;
+            }
+            
+            let upcomingEventsLine = '';
+            if (Array.isArray(club.upcoming_events)) {
+                upcomingEventsLine = `<li><strong>Upcoming Events:</strong> `;
+                upcomingEventsLine += club.upcoming_events.map(event => {
+                    if (event.link) {
+                        return `<a href="${event.link}" target="_blank">${event.name}</a>`;
+                    } else {
+                        return event.name;
+                    }
+                }).join(", ");
+                upcomingEventsLine += `</li>`;
+            }
+            
+            let clubItem = `
+            <div class="card">
+                <div class="card-header" id="headingCollege${club.id}">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCollege${club.id}" aria-expanded="false" aria-controls="collapseCollege${club.id}" id="btnCollege${club.id}">
+                            <span class="plus-sign"></span> ${club.name} (${club.number}) - ${club.city}, ${club.state}
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseCollege${club.id}" class="collapse" aria-labelledby="headingCollege${club.id}" data-parent="#college-club-list">
+                    <div class="card-body">
+                        <ul>
+                            <li><strong>Name:</strong> ${club.name}</li>
+                            <li><strong>Club Number:</strong> ${club.number}</li>
+                            ${teamNumberLine}
+                            <li><strong>Type:</strong> ${club.club_type}</li>
+                            <li><strong>City:</strong> ${club.city}</li>
+                            <li><strong>State:</strong> ${club.state}</li>
+                            <li><strong>Website:</strong> <a href="${club.website}" target="_blank">${club.website}</a></li>
+                            <li><strong>President:</strong> ${club.president}</li>
+                            <li><strong>Faculty Advisor:</strong> ${club.faculty_advisor}</li>
+                            <li><strong>Mentor:</strong> ${club.mentor}</li>
+                            <li><strong>Flyer of Record:</strong> ${club.flyer_of_record}</li>
+                            ${associatedLaunchClubsLine}
+                            <li><strong>Contact Email:</strong> <a href="mailto:${club.contact_email}">${club.contact_email}</a></li>
+                            ${upcomingEventsLine}
+                        </ul>
+                    </div>
+                </div>
+            </div>`;
+            
+            $('#college-club-list').append(clubItem);
+        });
+        
+        // Plus/minus sign toggling
+        $('.collapse').on('show.bs.collapse', function () {
+            $(this).prev('.card-header').find('.plus-sign').removeClass('plus-sign').addClass('minus-sign');
+        }).on('hide.bs.collapse', function () {
+            $(this).prev('.card-header').find('.minus-sign').removeClass('minus-sign').addClass('plus-sign');
+        });
+    }).fail(function (jqxhr, textStatus, error) {
+        console.error("Failed to load college clubs:", textStatus, error);
+    });
+});
--- assets/js/launch-clubs.js
+++ assets/js/launch-clubs.js
@@ -1,29 +1,27 @@
-$(document).ready(function() {
-    console.log("Document is ready");
+$(document).ready(function () {
+    console.log("Loading Launch Clubs");
     
-    $.getJSON("/assets/json-data/club-info.json", function(data) {
-        console.log("JSON data loaded", data);
+    $.getJSON("/assets/json-data/launch-clubs.json", function (data) {
+        console.log("Launch Clubs JSON loaded", data);
         
-        data.forEach(function(club) {
-            console.log("Processing club:", club);
-            
+        data.forEach(function (club) {
             let clubItem = `
             <div class="card">
-                <div class="card-header" id="heading${club.id}">
+                <div class="card-header" id="headingLaunch${club.id}">
                     <h2 class="mb-0">
-                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${club.id}" aria-expanded="true" aria-controls="collapse${club.id}" id="btn${club.id}">
+                        <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLaunch${club.id}" aria-expanded="false" aria-controls="collapseLaunch${club.id}" id="btnLaunch${club.id}">
                             <span class="plus-sign"></span> ${club.name} (${club.number}) - ${club.city}, ${club.state}
                         </button>
                     </h2>
                 </div>
-                <div id="collapse${club.id}" class="collapse" aria-labelledby="heading${club.id}" data-parent="#club-list">
+                <div id="collapseLaunch${club.id}" class="collapse" aria-labelledby="headingLaunch${club.id}" data-parent="#launch-club-list">
                     <div class="card-body">
                         <ul>
                             <li><strong>Name:</strong> ${club.name}</li>
                             <li><strong>Club Number:</strong> ${club.number}</li>
                             <li><strong>Type:</strong> ${club.club_type}</li>
                             <li><strong>City:</strong> ${club.city}</li>
-                            <li><strong>State:</strong> ${club.state}</li><li><strong>State:</strong> ${club.state}</li>
+                            <li><strong>State:</strong> ${club.state}</li>
                             <li><strong>Website:</strong> <a href="${club.website}" target="_blank">${club.website}</a></li>
                             <li><strong>Launch Site:</strong> ${club.launch_site}</li>
                             <li><strong>Launch Site Location:</strong> ${club.launch_site_location}</li>
@@ -35,23 +33,15 @@ $(document).ready(function() {
                 </div>
             </div>`;
             
-            $('#club-list').append(clubItem);
+            $('#launch-club-list').append(clubItem);
         });
         
-        console.log("All clubs processed and added to the DOM");
-        
-        // Add event listeners to buttons
-        $('[id^=btn]').click(function() {
-            console.log(`Button ${this.id} clicked`);
-        });
-        
-        // Add event listeners to toggle plus/minus signs
-        $('.collapse').on('show.bs.collapse', function() {
+        $('.collapse').on('show.bs.collapse', function () {
             $(this).prev('.card-header').find('.plus-sign').removeClass('plus-sign').addClass('minus-sign');
-        }).on('hide.bs.collapse', function() {
+        }).on('hide.bs.collapse', function () {
             $(this).prev('.card-header').find('.minus-sign').removeClass('minus-sign').addClass('plus-sign');
         });
-    }).fail(function(jqxhr, textStatus, error) {
-        console.error("Error loading JSON data", textStatus, error);
+    }).fail(function (jqxhr, textStatus, error) {
+        console.error("Failed to load launch clubs:", textStatus, error);
     });
 });
