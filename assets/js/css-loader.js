1cfff6160180b6bfe10dda0765c1c0e5ca2cf3a6
Chris Maier <maier.chris@gmail.com>
2026-01-07 23:57:47 -0600

    Fixing the load order of the site and optimizing load speed

--- assets/js/css-loader.js
+++ assets/js/css-loader.js
@@ -1,3 +1,4 @@
+// css-loader.js
 
 /*
     Dynamically load CSS files from a json file
@@ -5,28 +6,97 @@
     by keeping the list of files that need to be included in a single location
     
     ** you can always add a single line to include one CSS file if the page requires a one-off CSS **
- */
+*/
 
 document.addEventListener('DOMContentLoaded', function()
 {
-    fetch('/assets/json-data/css-head-config.json')
-        .then(response => response.json())
-        .then(data => {
-            const head = document.head;
-            
-            // Link tags
-            data.links.forEach(linkData => {
-                const link = document.createElement('link');
-                Object.keys(linkData).forEach(key => {
-                    link.setAttribute(key, linkData[key]);
-                });
-                head.appendChild(link);
-            });
-        })
-        .catch(error => {
-            console.error('Error loading the CSS configuration:', error);
-        });
-    
-    // Make the body of the page visible after all the css files have been loaded
-    document.body.style.display = "block";
+    loadCssConfigAndRevealBody();
 });
+
+async function loadCssConfigAndRevealBody()
+{
+    const cssConfigUrl = '/assets/json-data/css-head-config.json';
+    const maxWaitMilliseconds = 3000;
+
+    try
+    {
+        const configData = await fetchCssConfig(cssConfigUrl);
+        await appendCssLinksAndWait(configData, maxWaitMilliseconds);
+    }
+    catch(error)
+    {
+        console.error('Error loading the CSS configuration:', error);
+    }
+
+    revealBodyAndSignalReady();
+}
+
+async function fetchCssConfig(cssConfigUrl)
+{
+    const response = await fetch(cssConfigUrl, { cache: 'no-store' });
+
+    if(!response.ok)
+    {
+        throw new Error('CSS config HTTP ' + response.status);
+    }
+
+    return await response.json();
+}
+
+async function appendCssLinksAndWait(configData, maxWaitMilliseconds)
+{
+    if(!configData || !Array.isArray(configData.links)) return;
+
+    const headElement = document.head;
+    const loadPromises = configData.links.map(function(linkAttributes)
+    {
+        return createStylesheetLinkPromise(headElement, linkAttributes);
+    });
+
+    await Promise.race([
+        Promise.allSettled(loadPromises),
+        createTimeoutPromise(maxWaitMilliseconds)
+    ]);
+}
+
+function createStylesheetLinkPromise(headElement, linkAttributes)
+{
+    return new Promise(function(resolve)
+    {
+        const linkElement = document.createElement('link');
+
+        Object.keys(linkAttributes || {}).forEach(function(attributeName)
+        {
+            linkElement.setAttribute(attributeName, linkAttributes[attributeName]);
+        });
+
+        linkElement.onload = function()
+        {
+            resolve(true);
+        };
+
+        linkElement.onerror = function()
+        {
+            resolve(false);
+        };
+
+        headElement.appendChild(linkElement);
+    });
+}
+
+function createTimeoutPromise(waitMilliseconds)
+{
+    return new Promise(function(resolve)
+    {
+        window.setTimeout(function()
+        {
+            resolve(false);
+        }, waitMilliseconds);
+    });
+}
+
+function revealBodyAndSignalReady()
+{
+    document.body.style.visibility = 'visible';
+    document.dispatchEvent(new Event('rg-css-ready'));
+}
--- assets/js/css-reload.js
+++ assets/js/css-reload.js
@@ -1,12 +1,12 @@
+// css-reload.js
+
 /*
     This file should be the VERY LAST js file at the bottom of the page
     The purpose of the script / function is to get the css objects to re-apply
     Therefore it needs to run as late in the load process as possible
- */
-document.addEventListener("DOMContentLoaded", function() {
-    // Apply your CSS dynamically here
-    // For example, you can add or remove classes to trigger changes
-    
-    // Example:
+*/
+
+document.addEventListener('rg-css-ready', function()
+{
     document.body.classList.add('loaded');
 });
